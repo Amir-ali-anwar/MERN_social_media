b1189f6f1da7c4dcdfaf32a228d45ffbba16c6bd
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-11-20 20:19:02 +0500

    working on the auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import StatusCodes from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import { BadRequestError } from "../errors/index.js";
 const register = async (req, res, next) => {
   const {
